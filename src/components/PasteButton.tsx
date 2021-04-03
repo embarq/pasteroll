ec404841011756b3539580743811c3347700350f
embarq <embarq.dev@gmail.com>
2021-04-03 07:39:50 +0300

    fix paste doesn't work in safari

--- src/components/PasteButton.tsx
+++ src/components/PasteButton.tsx
@@ -4,22 +4,29 @@ import { clipboardOutline } from 'ionicons/icons'
 import { nanoid } from 'nanoid'
 import { Plugins } from '@capacitor/core'
 import { ClipboardItems } from '../lib/state'
-import { isString } from '../lib/utils'
+import { isString, maybeReadFromWebClipboard } from '../lib/utils'
 
 const PasteButton: React.FC = () => {
   const items = useHookstate(ClipboardItems)
 
   async function handleClick(): Promise<void> {
-    const { value } = await Plugins.Clipboard.read()
+    try {
+      const value =
+        navigator.clipboard != null
+          ? await maybeReadFromWebClipboard()
+          : await Plugins.Clipboard.read().then((res) => res.value)
 
-    if (isString(value)) {
-      items.merge([
-        {
-          id: nanoid(),
-          content: value,
-          created_at: Date.now(),
-        },
-      ])
+      if (isString(value)) {
+        items.merge([
+          {
+            id: nanoid(),
+            content: value,
+            created_at: Date.now(),
+          },
+        ])
+      }
+    } catch (err) {
+      console.error(err)
     }
   }
 
